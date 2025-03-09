26a4b6b5969aa6ab13a73ec5a0d4b0a3ab468576
Viniciusss1 <150733111+Viniciusss1@users.noreply.github.com>
2025-03-09 19:24:42 -0300

    Add files via upload

--- Adotebemteste/js/script.js
+++ Adotebemteste/js/script.js
@@ -50,3 +50,25 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+
+document.addEventListener("DOMContentLoaded", function() {
+    var userIcon = document.querySelector(".user-icon");
+    var userDropdown = document.getElementById("userDropdown");
+
+    // Exibir/Ocultar o modal ao clicar na imagem do usuário
+    userIcon.addEventListener("click", function(event) {
+        event.stopPropagation(); // Impede que o clique feche imediatamente o modal
+        userDropdown.style.display = userDropdown.style.display === "block" ? "none" : "block";
+    });
+
+    // Fechar o modal ao clicar fora dele
+    document.addEventListener("click", function(event) {
+        if (!userDropdown.contains(event.target) && event.target !== userIcon) {
+            userDropdown.style.display = "none";
+        }
+    });
+});
+
+
+
+
